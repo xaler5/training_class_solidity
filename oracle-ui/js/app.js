3d99d046c00811e292201b7a21349b467899c37c
Dario Lo Buglio <dariolobuglio@gmail.com>
2019-02-25 23:32:46 +0100

    adding web3

--- oracle-ui/js/app.js
+++ oracle-ui/js/app.js
@@ -1,20 +1,37 @@
 angular
     .module('oracleUi', [])
     .controller('oracleUiController', function ($http, $timeout) {
+
         var oracleUi = this;
+        var oracleJson;
 
         oracleUi.step = 1;
         console.log(oracleUi);
 
-
         $http.get("./Oracle.json")
-            .then(function(oracleJson) {
-                console.log(oracleJson);
+            .then(function(json) {
+                oracleJson = json;
+                console.log(json);
             })
 
         
         oracleUi.connect = function (event, host, address) {
             event.preventDefault();
+            //here host and address are undefined
+            console.log("HELLO");
+            console.log(event);
+            console.log(host);
+            console.log(address);
+            console.log(oracleUi.host);
+            console.log(oracleUi.address);
+            web3 = new Web3(new Web3.providers.HttpProvider(host));
+            console.log(web3);
+
+            var oracleContract = new web3.eth.contract(
+                oracleJson.data.abi,
+                address
+            );
+            console.log(oracleContract);
 
             oracleUi.connecting = true;
             console.log('connected');
